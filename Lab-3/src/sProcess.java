0617b1186aad98c61d4bbd20e77cabd98bf4c463
DarynaBond <35077916+WAG13@users.noreply.github.com>
2020-11-15 22:22:22 +0200

    added Process

--- Lab-3/src/sProcess.java
+++ Lab-3/src/sProcess.java
@@ -1,15 +1,19 @@
-public class sProcess {
-  public int cputime;
-  public int ioblocking;
-  public int cpudone;
-  public int ionext;
-  public int numblocked;
 
-  public sProcess (int cputime, int ioblocking, int cpudone, int ionext, int numblocked) {
-    this.cputime = cputime;
-    this.ioblocking = ioblocking;
-    this.cpudone = cpudone;
-    this.ionext = ionext;
-    this.numblocked = numblocked;
-  } 	
+public class sProcess implements Comparable{
+    public int cputime;
+    public int ioblocking;
+    public int priority;
+    public int cpudone;
+    public int ionext;
+    public int numblocked;
+
+    public sProcess (int cputime, int ioblocking, int priority, int cpudone, int ionext, int numblocked) {
+        this.cputime = cputime;
+        this.ioblocking = ioblocking;
+        this.priority = priority;
+        this.cpudone = cpudone;
+        this.ionext = ionext;
+        this.numblocked = numblocked;
+    }
+
 }
